9fe1ea2eedc6d43e5109480c56e3022b01fb099e
PiotrSzczachor1 <piotr.szczachor1@wp.pl>
2022-05-05 00:05:32 +0200

    Added TestInput Directory, changed keywords tokens, added ignoring comments and reading input from file

--- translator/tokens.py
+++ translator/tokens.py
@@ -2,103 +2,123 @@ from ply import lex
 from ply import yacc
 
 
+reserved = {
+    'for': 'FOR',
+    'while': 'WHILE',
+    'public': 'PUBLIC',
+    'private': 'PRIVATE',
+    'protected': 'PROTECTED',
+    'int': 'INT',
+    'char': 'CHAR',
+    'string': 'STRING',
+    'bool': 'BOOL',
+    'float': 'FLOAT',
+    'long': 'LONG',
+    'short': 'SHORT',
+    'unsigned': 'UNSIGNED',
+    'double': 'DOUBLE',
+    'class': 'CLASS',
+    'break': 'BREAK',
+    'switch': 'SWITCH',
+    'case': 'CASE',
+    'if': 'IF',
+    'else': 'ELSE',
+    'return': 'RETURN',
+    'true': 'TRUE',
+    'false': 'FALSE',
+    'include': 'INCLUDE',
+    'using': 'USING',
+    'namespace': 'NAMESPACE',
+    'std': 'STD',
+    'void': 'VOID',
+}
+
 tokens = [
-
-    'FOR',
-    'WHILE',
-    'PUBLIC',
-    'PRIVATE',
-    'PROTECTED',
-    'INT',
-    'CHAR',
-    'STRING',
-    'BOOL',
-    'FLOAT',
-    'LONG',
-    'SHORT',
-    'UNSIGNED',
-    'DOUBLE',
-    'CLASS',
-    'BREAK',
-    'SWITCH',
-    'CASE',
-    'IF',
-    'ELSE',
-    'RETURN',
-    'TRUE',
-    'FALSE',
-    'INCLUDE',
-    'USING',
-    'NAMESPACE',
-    'STD',
-    'VOID',
-
+    # operators and signs
+    'COMMENT',
+    'PLUS_PLUS',
+    'MINUS_MINUS',
+    'EQUAL_EQUAL',
     'PLUS',
     'MINUS',
     'MULTIPLY',
     'DIVIDE',
     'EQUAL',
-    'PLUS_PLUS',
-    'MINUS_MINUS',
-    'EQUAL_EQUAL',
     'LEFT_BR',
     'RIGHT_BR',
     'LEFT_BR_SQUARED',
     'RIGHT_BR_SQUARED',
     'LEFT_BR_CURLY',
     'RIGHT_BR_CURLY',
+    'SEMICOLON',
+    # numbers and variables
+    'INT_NUMBER',
+    'FLOAT_NUMBER',
+    'VAR',
+    # ID for keywords
+    'ID'
+] + list(reserved.values())
 
-    'NUMBER',
-    'VAR'
 
-]
+def t_comment(t):
+    r'\//.*'
+    pass
+    # No return value. Token discarded
 
-t_FOR = 'for'
-t_WHILE = 'while'
-t_PUBLIC = 'public'
-t_PRIVATE = 'private'
-t_PROTECTED = 'protected'
-t_INT = 'int'
-t_CHAR = 'char'
-t_STRING = 'string'
-t_BOOL = 'bool'
-t_FLOAT = 'float'
-t_LONG = 'long'
-t_SHORT = 'short'
-t_UNSIGNED = 'unsigned'
-t_DOUBLE = 'double'
-t_CLASS = 'class'
-t_BREAK = 'break'
-t_SWITCH = 'switch'
-t_CASE = 'case'
-t_IF = 'if'
-t_ELSE = 'else'
-t_RETURN = 'return'
-t_TRUE = 'true'
-t_FALSE = 'false'
-t_INCLUDE = 'include'
-t_USING = 'using'
-t_NAMESPACE = 'namespace'
-t_STD = 'std'
-t_VOID = 'void'
 
+t_PLUS_PLUS = r'\++'
+t_MINUS_MINUS = r'--'
+t_EQUAL_EQUAL = r'=='
 t_PLUS = r'\+'
 t_MINUS = r'\-'
 t_MULTIPLY = r'\*'
 t_DIVIDE = r'\/'
 t_EQUAL = r'\='
-t_PLUS_PLUS = r'\++'
-t_MINUS_MINUS = r'--'
-t_EQUAL_EQUAL = r'=='
 t_LEFT_BR = r'\('
 t_RIGHT_BR = r'\)'
 t_LEFT_BR_SQUARED = r'\['
 t_RIGHT_BR_SQUARED = r'\]'
 t_LEFT_BR_CURLY = r'\{'
 t_RIGHT_BR_CURLY = r'\}'
+t_SEMICOLON = r'\;'
 
-t_NUMBER = r'(\d+(?:\.\d+)?)'
+
+t_ignore = ' '
+
+
+def t_FLOAT_NUMBER(t):
+    r'\d+\.\d+'
+    t.value = float(t.value)
+    return t
+
+
+def t_INT_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+
+def t_ID(t):
+    r'[a-zA-Z][a-zA-Z]*'
+    t.type = reserved.get(t.value, 'VAR')    # Check for reserved words if not in reserved words than its VAR
+    return t
+
+
 t_VAR = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
+with open('TestInput\input2') as f:
+    lines = f.readlines()
 
+code = "".join(lines)
+
+lexer = lex.lex()
+
+lexer.input(code)
+
+while True:
+    tok = lexer.token()
+    if not tok:
+        break
+    else:
+        print(tok)
 
